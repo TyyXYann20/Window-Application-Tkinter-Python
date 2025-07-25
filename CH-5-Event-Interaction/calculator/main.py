3213ea9c42ceb7b518f644a13dc9416a1636175b
TyyXYann20 <saiyannsic20@gmail.comcls>
2025-07-25 22:54:24 +0700

    Calculator APP Part-2

--- CH-5-Event-Interaction/calculator/main.py
+++ CH-5-Event-Interaction/calculator/main.py
@@ -15,6 +15,55 @@ main_frme.grid(sticky="news")
 main_frme.rowconfigure((0,1,2,3,4), weight=1)
 main_frme.columnconfigure((0,1,2,3), weight=1)
 
+# Function Section
+expression = ""
+OPS_SET = {"*", "/", "+", "-"}
+
+def handle_key(event):
+    key = event.char
+    if key.isdigit():
+        append_value(key)
+    elif key in OPS_SET:
+        if key == "*":
+            append_value( "x")
+        elif key == "/":
+            append_value("÷")
+        else:
+            append_value(key)
+    else:
+        pass
+        
+
+def append_value(val):
+    
+    global expression
+    if val in ["0", "Error"]:
+        expression = val #  0, 12+
+    else:
+        expression += val
+    display_var.set(expression)
+    
+        
+def on_click(button):
+    if button == "AC":
+        clear_result()
+    elif button == "=":
+        #calculate()
+        print("User pressed = Button")
+        pass
+    elif button == "%":
+        pass
+    elif button == "√":
+        pass
+    
+    else:
+        append_value(button) 
+        
+def clear_result():
+    global expression
+    expression = ""
+    display_var.set("0")
+
 display_var = tk.StringVar(value="0")
 display_res = tk.Label(
     main_frme, 
@@ -55,9 +104,9 @@ for index, value in enumerate(BUTTONS_VALUE):
         borderwidth=0,
         font=("Arial", 30),
         cursor="hand2",
-        command=None
+        command= lambda v=value: on_click(v)
     )
     btn.grid(row=row+1, column=col, padx=1, pady=1, sticky="news")
 
-
+window.bind("<Key>", handle_key)
 window.mainloop()
